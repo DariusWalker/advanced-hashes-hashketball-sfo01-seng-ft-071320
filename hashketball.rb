b174148cf2a4335d8194e864035a8d8f1bf2b5d8
Darius Walker <dw862716@ohio.edu>
2020-07-13 20:31:29 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -127,25 +127,25 @@ def game_hash
   }
 end
 
-def num_points_scored(player_name)
-  game_hash.each do |location, team_data|
-    team_data[:players].each do |player|
-      if player[:player_name] == player_name
-       return player[:points]
-      end
-    end
-  end
-end
+        def num_points_scored(player_name)
+          game_hash.each do |location, team_data|
+            team_data[:players].each do |player|
+              if player[:player_name] == player_name
+               return player[:points]
+              end
+            end
+          end
+        end
 
-def shoe_size(player_name)
-  game_hash.each do |location, team_data|
-    team_data[:players].each do |player|
-      if player[:player_name] == player_name
-       return player[:shoe]
-      end
-    end
-  end
-end
+        def shoe_size(player_name)
+          game_hash.each do |location, team_data|
+            team_data[:players].each do |player|
+              if player[:player_name] == player_name
+               return player[:shoe]
+              end
+            end
+          end
+        end
 
         def team_colors(team_name)
           game_hash.each do |location, team_data|
